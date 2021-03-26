3fea06fb54fea08a65facb9fb104037ff626eca0
patel <aiufghaweiyugiaweul>
2021-03-25 21:05:10 -0400

    Finished Lab7

--- app/src/main/java/com/cst2335/pate0844/DetailsFragmentt.java
+++ app/src/main/java/com/cst2335/pate0844/DetailsFragmentt.java
@@ -1,5 +1,6 @@
 package com.cst2335.pate0844;
 
+
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +10,6 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -17,6 +17,7 @@ import androidx.fragment.app.Fragment;
  * A simple {@link Fragment} subclass.
  * Use the {@link DetailsFragmentt#newInstance} factory method to
  * create an instance of this fragment.
+ *
  */
 public class DetailsFragmentt extends Fragment {
 
@@ -24,16 +25,13 @@ public class DetailsFragmentt extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private Bundle dataFromActivity;
+    private long id;
+    private AppCompatActivity parentActivity;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private Long mParam2;
-
-    private AppCompatActivity parentActivity;
-
-    public DetailsFragmentt() {
-        // Required empty public constructor
-    }
+    private String mParam2;
 
     /**
      * Use this factory method to create a new instance of
@@ -41,7 +39,7 @@ public class DetailsFragmentt extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
+     * @return A new instance of fragment DetailsFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static DetailsFragmentt newInstance(String param1, String param2) {
@@ -53,41 +51,58 @@ public class DetailsFragmentt extends Fragment {
         return fragment;
     }
 
+    public DetailsFragmentt() {
+        // Required empty public constructor
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getLong(ARG_PARAM2);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        parentActivity= (AppCompatActivity) context;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        String msg=getArguments().getString("msg");
-        Long id=getArguments().getLong("id");
-        Boolean send=getArguments().getBoolean("send");
-        View view=inflater.inflate(R.layout.activity_details_fragmentt, container, false);
-        TextView msgText=view.findViewById(R.id.Messagelbl);
-        TextView idText=view.findViewById(R.id.Messagelbl);
-        CheckBox sendBox=view.findViewById(R.id.IsSend);
+        //return inflater.inflate(R.layout.fragment_details, container, false);
 
-        msgText.setText(msg);
-        idText.setText(id+"");
-        sendBox.setChecked(send);
 
-        Button hideButton=view.findViewById(R.id.Hidebtn);
-        hideButton.setOnClickListener(v->{
+        dataFromActivity = getArguments();
+        id = dataFromActivity.getLong(ChatRoomActivity.ITEM_ID );
+
+        // Inflate the layout for this fragment
+        View result =  inflater.inflate(R.layout.activity_details_fragmentt, container, false);
+
+        //show the message
+        TextView message = (TextView)result.findViewById(R.id.Messagelbl);
+        message.setText(dataFromActivity.getString(ChatRoomActivity.ITEM_SELECTED));
+
+        //show the id:
+        TextView idView = (TextView)result.findViewById(R.id.IDlbl);
+        idView.setText("ID=" + id);
+
+        CheckBox checkBox = result.findViewById(R.id.IsSend);
+        checkBox.setChecked(dataFromActivity.getBoolean(ChatRoomActivity.ITEM_POSITION));
+        // get the delete button, and add a click listener:
+        Button finishButton = result.findViewById(R.id.Hidebtn);
+        finishButton.setOnClickListener( clk -> {
+
+            //Tell the parent activity to remove
             parentActivity.getSupportFragmentManager().beginTransaction().remove(this).commit();
+
+            if (ChatRoomActivity.IsPhone) getActivity().onBackPressed();
         });
-        return view;
+
+        return result;
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        //context will either be FragmentExample for a tablet, or EmptyActivity for phone
+        parentActivity = (AppCompatActivity)context;
     }
 }
